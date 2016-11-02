b1785f69d5fb3640c30553c129c1a89685978c9c
dimalunin2016 <dimon2016@mail.ru>
2016-11-02 20:31:56 +0300

    Add files via upload

--- test.h
+++ test.h
@@ -12,7 +12,7 @@ bool comp(int a, int b)
     return a < b;
 }
 template <class RandomAccessIterator, class T = typename std::iterator_traits <RandomAccessIterator>::value_type, class Compare>
-  void check(RandomAccessIterator first, RandomAccessIterator last, bool writeTime, Compare comp){
+  void check(RandomAccessIterator first, RandomAccessIterator last, Compare comp){
         int n = last - first;
         T* copy = new T[n];
         std::copy(first, last, copy);
@@ -24,15 +24,14 @@ template <class RandomAccessIterator, class T = typename std::iterator_traits <R
         double tSort = (t2 - t1) / CLOCKS_PER_SEC;
         double tTimsort = (t3 - t2) / CLOCKS_PER_SEC;
         for(int i = 0; i < n; i++) {
-                //std::cout << copy[i] <<" ";
+                //std::cout << copy[i] <<"\n";
             if(copy[i] != first[i]) {
                 std::cout <<"It works incorrectly, but it does not matter, just forget about this test)\n";
                 break;
             }
         }
         //std::cout << tSort <<" "<<tTimsort<<"-\n";
-        if(writeTime)
-            printf("Sort time: %f\nTimSort time: %f\n---------------------\n", tSort, tTimsort);
+        printf("Sort time: %f\nTimSort time: %f\n---------------------\n", tSort, tTimsort);
         delete[] copy;
   }
 void fillArray(int *a, int n) {
@@ -43,11 +42,12 @@ void fillArray(int *a, int n) {
 void testRandomArray() {
     std::srand(std::time(0));
     int n[11] = {1, 2, 4, 8, 16, 32, 50, 100, 1000, 10000, 100000};
-    for (int i = 0; i < 11; i++) {
+    for (int j = 0; j < 110; j++) {
+        int i = j % 11;
         int *a = makeArray(n[i]);
         fillArray(a, n[i]);
         std::cout <<"length: "<<  n[i] <<"\n";
-        check(a, a + n[i], 1, comp);
+        check(a, a + n[i], comp);
         delete[] a;
     }
 }
@@ -66,7 +66,7 @@ void testPartiallySortedArray() {
                     std::reverse(a + size[i] * k, a + size[i] * (k + 1));
             }
             std::cout <<"length: "<<  size[i] * lenOfSortedArrays[j] <<"\n";
-            check(a, a + size[i] * lenOfSortedArrays[j], 1, comp);
+            check(a, a + size[i] * lenOfSortedArrays[j], comp);
             delete[] a;
         }
     }
@@ -78,19 +78,20 @@ bool stringComp(std::string a, std::string b) {
 }
 void testString() {
     std::srand(std::time(0));
-    const int size = 80;
+    const int size = 1600;
     std::string a[size];
     int tests = 10;
     for(int k = 0; k < tests; k++) {
         for (int i = 0; i < size; ++i) {
-            int len = std::rand() % 100;
+            int len = std::rand() % 1000 + 1;
             std::string s = "";
             for(int j = 0; j < len; j++) {
-                s+=rand() % 80;
+                s+=rand() % 26 + 'a';
             }
             a[i] = s;
         }
-        check(a, a + size, 0, stringComp);
+        std::cout <<"length: "<< size <<"\n";
+        check(a, a + size, stringComp);
     }
 }
 
